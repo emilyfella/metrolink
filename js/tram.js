c465b43f9e09949c5eb941a872134ca76740bc39
Oscar Tracey-Prior <oscartraceyprior@gmail.com>
2017-10-10 15:21:29 +0100

    drivings

--- js/tram.js
+++ js/tram.js
@@ -1,1 +1,23 @@
-//super loads of cats
+function Tram(route, startingStation) {
+  this._route = route
+  this._currentStation = startingStation
+}
+
+Tram.prototype = {
+  getCurrentStation: function() {
+    return this._currentStation
+  },
+
+  drive: function() {
+    var routeStations = this._route.getStations()
+    var currentStationIndex = routeStations.indexOf(this._currentStation)
+    var nextStationIndex = currentStationIndex + 1
+
+    if(nextStationIndex >= routeStations.length) {
+      return "End of the line."
+    }
+
+    this._currentStation = routeStations[nextStationIndex]
+    return "Now arriving at: " + this._currentStation.getName()
+  }
+}
