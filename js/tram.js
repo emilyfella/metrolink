9571f690a32d94b3da86f2b1cc9e2d04a7139b87
Oscar Tracey-Prior <oscartraceyprior@gmail.com>
2017-10-10 16:19:17 +0100

    V4
    ADDED SICK STUFF

--- js/tram.js
+++ js/tram.js
@@ -8,16 +8,26 @@ Tram.prototype = {
     return this._currentStation
   },
 
+  getStartingStation: function()  {
+    return this._route.getStations()[0].getName()
+  },
+
+  getTerminatingStation: function() {
+    return this._route.getStations()[this._route.getStations().length-1].getName()
+  },
+
   drive: function() {
     var routeStations = this._route.getStations()
     var currentStationIndex = routeStations.indexOf(this._currentStation)
     var nextStationIndex = currentStationIndex + 1
+    var arrivingAt = " Now arriving at: " + this._currentStation.getName()
+    var routeAnnouncement = "This is the service from " + this.getStartingStation() + " to " + this.getTerminatingStation() + "."
 
     if(nextStationIndex >= routeStations.length) {
       return "End of the line."
     }
 
     this._currentStation = routeStations[nextStationIndex]
-    return "Now arriving at: " + this._currentStation.getName()
+    return (routeAnnouncement + arrivingAt)
   }
 }
